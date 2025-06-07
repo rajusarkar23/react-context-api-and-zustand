ece87dde83bcaca09e82e4aface72f410a4376a2
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-06-07 13:36:30 +0530

    added old code in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import AddToCartButton from "./components/AddToCartButton";
 import LoginButton from "./components/LoginButton";
 import { useUserStore } from "./store/useUserStore";
 
+// get the theme provider from theme context
+// import { ThemeProvider } from "./context/ThemeContext";
+// get the theme switch component
+// import ThemeSwitch from "./components/ThemeSwitch";
+// import DemonstrateThemeChange from "./components/DemonstrateThemeChange";
+
 const App = () => {
   const {isUserLogedIn}=  useUserStore()
   return (
+
+    // <ThemeProvider>
+    //   <ThemeSwitch />
+    //   <DemonstrateThemeChange />
+    // </ThemeProvider>
+
+    
     <div>
       <LoginButton />
 
